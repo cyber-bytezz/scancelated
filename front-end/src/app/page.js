add74e6ce5d6573549c3bf5efdd08b726f3babe2
Aro Barath Chandru B <chandru2021007@gmail.com>
2024-11-12 19:10:21 +0530

    Deleted Unwanted Files

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import MedicalTerm from './medical-term'
-import TextRecognition from './text-recognition'
-import TextRecognitionCamera from './text-camera'
 import SingleMedicalTerm from './singlemedical-term'
 
 const Page = () => {
   return (
     <div>
-      {/* <MedicalTerm/> */}
       <SingleMedicalTerm/>
-      {/* <TextRecognition/> */}
-      {/* <TextRecognitionCamera /> */}
     </div>
   )
 }
